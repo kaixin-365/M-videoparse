d72d1f9e39991a644571897a3a4d7157f13a476c
kaixin <1229738486@qq.com>
2023-02-16 20:27:34 +0800

    更新好看视频取流api

--- python-backed/old/haokan.py
+++ python-backed/old/haokan.py
@@ -1,33 +1,41 @@
 from httpx import AsyncClient
+from json import loads
+from lxml import etree
 from urllib.parse import urlsplit
 from random import randint
 from router.router import router
 from services.redis import haokan_redis
 from fastapi import Response
 
-client = AsyncClient(http2=True)
+client = AsyncClient()
 
 async def getvideolink(vid: int):
-    url = 'https://sv.baidu.com/appui/api'
+    url = 'http://vv.baidu.com/v'
     headers = {
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.8,*/*;q=0.8',
         'cache-control': 'no-cache',
         'Connection': 'keep-alive',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
     }
-    html = await client.get(url=url, params={'cmd':'video/relate','vid':vid}, headers=headers, timeout=5)
-    html = html.json()
-    try:
-        video_json = html['video/relate']['data']['cur_video']['video_list']
-    except:
-        raise Exception('此视频不存在！')
-    qualitys = ['1080p','sc','hd','sd']
+    html = await client.get(url=url, params={'vid':vid}, headers=headers, timeout=5)
+    data = etree.HTML(html.text)
+    data = data.xpath('//*[@id="_page_data"]')
+    info_json = str(data[0].text).split('_ = ')[1]
+    info_json = loads(info_json.split(';')[0])['curVideoMeta']['clarityUrl']
+
+    qualitys = ['蓝光','超清','高清','标清']
+    url_dict = {}
+    for i in info_json:
+        url_split = urlsplit(i['url'])
+        url = f'{url_split[0]}://{url_split[1]}{url_split[2]}'
+        quality = i['title']
+        url_dict[quality] = url
 
     for k in qualitys:
-        if k in video_json:
-            url = video_json[k]
-
-    return url
+        try:
+            return url_dict[k]
+        except:
+            pass
 
 async def set_cache(vid: int, value: str):
     try:
@@ -62,6 +70,5 @@ async def haokan_main(vid: int):
             return '获取链接出错！'
         finally:
             value = urlsplit(url)[2]
-            print(value)
             await set_cache(vid,value)
 
